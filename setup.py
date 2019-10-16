02fbee6e935b9990e98874925b091e07ff89bdb0
Brendan C. Ward <bcward@astutespruce.com>
2019-10-16 13:59:08 -0700

    Bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="pymbtiles",
-    version="0.4.0",
+    version="0.5.0",
     packages=["pymbtiles"],
     url="https://github.com/consbio/pymbtiles",
     license="ISC",
@@ -13,8 +13,6 @@ setup(
     long_description_content_type="text/markdown",
     long_description=open("README.md").read(),
     install_requires=[],
-    extras_require={
-        'test': ['pytest', 'pytest-cov'],
-    },
+    extras_require={"test": ["pytest", "pytest-cov"]},
     include_package_data=True,
 )
